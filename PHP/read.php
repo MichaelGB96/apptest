26c11665bc0084a02ba93c3aa265130241db878a
MichaelGB96 <MichaelGB96@users.noreply.github.com>
2023-09-23 10:13:59 +0200

    escribir codigo para la lectura de datos

--- PHP/read.php
+++ PHP/read.php
@@ -1,8 +1,23 @@
 <?php
 
-    require_once("./producto.php");
+require_once("./producto.php");
 
+//Si se especifica un id, se mostrará solo 1 producto
+if (!empty($_POST['id'])) {
+    $idProducto = $_POST['id'];
+
+    $producto = new Producto();
+    $producto->getById($idProducto);
+
+/*     $nombre = $producto->getNombre();
+    $tipo = $producto->getTipo();
+    $precio = $producto->getPrecio(); */
+
+    $json = json_encode($producto);
+       
+} else { // Si no se especifica id, se muestran todos los productos
     $productos = new Producto();
     $json = json_encode($productos->getProductos());
+}
 
-?>
+echo $json;
